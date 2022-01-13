9eaf758317ba60972805aba4f4b37a1e24c3b673
Denis Kotelev <hardsmile98@mail.ru>
2022-01-13 09:48:44 +0300

    fixed version ts

--- src/hooks/useHttp.ts
+++ src/hooks/useHttp.ts
@@ -11,7 +11,7 @@ const useHttp = () => {
   const request: IRequest = useCallback(
     async (url, method = 'GET', body = null, headers = {}) => {
       try {
-        const baseUrl = 'https://nameless-sea-96063.herokuapp.com' + url
+        const baseUrl = 'https://evening-citadel-01767.herokuapp.com' + url
         if (body) {
           body = JSON.stringify(body)
           headers['Content-Type'] = 'application/json'
@@ -29,7 +29,7 @@ const useHttp = () => {
         }
 
         return data
-      } catch (e) {
+      } catch (e: any) {
         setError(e.message)
         throw e
       }
